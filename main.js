f8e3a201e01df15123d44729dfea39f57632244a
ahmedelgamasy <ahmedalgamasyy@gmail.com>
2024-08-19 15:36:56 +0300

    19/8/24

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 /* challange */
 
-let a = 10;
-let b = "20"
-let c = 80;
+let a = 1_00;
+let b = 2_00.5
+let c = 1e2;
+let d = 2.4;
 
-console.log(++a + + b ++ + +c++ - +a++)
-console.log(++a + + b ++ + +c++ - +a++ +a )
-console.log(++a + + b ++ + +c++ - +a++ - +true)
+console.log(Math.round(Math.ceil()))
