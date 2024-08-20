2652eed4efebe85a176aa13f52b5cb6b011495d4
ahmedelgamasy <ahmedalgamasyy@gmail.com>
2024-08-20 23:51:31 +0300

    20/8/24

--- main.js
+++ main.js
@@ -1,21 +1,6 @@
 
 
-let thename = " Ahmed  ";
-let theList =  [1, 2, 3, 4, 5];
+let myfriends = ["Ahmed", "sayed", "ali", "mody"];
+let mynewfriends  =  ["samar", "sameh"];
+let schoolfriends = ["Haythem", "shady"];
 
-console.log(thename);
-console.log(thename[1]);
-console.log(thename[5]);
-
-console.log(thename.charAt(1));
-console.log(thename.charAt(5));
-
-console.log(thename.length);
-
-console.log(thename.trim());
-
-console.log(thename.toUpperCase())
-
-console.log(10 < 20 );
-console.log(10 > 30 );
-console.log(10 < 5 );
