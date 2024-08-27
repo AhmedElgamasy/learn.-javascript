d7daa2b9e3ebfb1d88f22501c20356880e543dbe
ahmedelgamasy <ahmedalgamasyy@gmail.com>
2024-08-27 22:35:22 +0300

    27/8/24

--- main.js
+++ main.js
@@ -1,6 +1,152 @@
+/*
+console.log(10== "10");
+console.log(-100 == "-100");
+console.log(10 != "10");
+
+console.log(10 === "10");
+console.log(10 !== "10");
+console.log(10 !== 10);
+console.log(10 > 20);
+console.log(10 > 10);
+console.log(10 >= 10);
+console.log(10 <= 10);
+*/
+/*
+console.log(true);
+console.log(!true);
+
+console.log((10 == "10"));
+
+console.log(10 == "10" && 10 > 8 && 10 > 50);
+
+console.log(10 == "10" || 10 > 8 || 10 > 50);
+*/
+
+/*
+let price = 100; 
+let discount = true;
+let discountAmount = 30;
+
+console.log(price);    
+
+if (discount === true) {
+  price -= discountAmount;
+  
+  price = price -  discountAmount;
+
+console.log(price);
+*/
+
+/*
+let price = 100;
+let discount = false;
+let discountAmount = 30;
+let country = "Egypt";
+let student = true;
+
+if (discount === true ) {
+
+    price -= discountAmount;
+
+} else if (country === "Egypt") {
+    price -= discountAmount + 10 ;
 
 
-let myfriends = ["Ahmed", "sayed", "ali", "mody"];
-let mynewfriends  =  ["samar", "sameh"];
-let schoolfriends = ["Haythem", "shady"];
 
+} else{
+
+    price -= 10;
+}
+
+console.log(price) 
+*/
+
+
+// let theName = "Ahmed";
+// let theGender = "Male";
+// let theAge = 30; 
+
+// if (theGender === "male" ){
+//     console.log("Mr");
+// } else {
+//     console.log("Mrs");
+// }
+
+// theGender === "Male" ? console.log("Mr") : console.log("Mrs");
+
+
+// let mybro = ["Ahmed", "Sayed", "Ali", "Osama", "Gamal","Ameer" ];
+// console.log(mybro)
+// console.log(mybro.slice());
+// console.log(mybro.slice(1, 3));
+// console.log(mybro.slice(-3));
+// console.log(mybro.slice(1, -2));
+ 
+  
+ 
+//  let myfriends = ["Ahmed" ];
+//  let mynewfrinds = ["samer", "sameh" ];
+//  let schoolfriends = ["haytham", "Shady"];
+
+//  let allfriends = myfriends.concat(mynewfrinds, schoolfriends, "Gamal", schoolfriends.slice(1, 3));
+
+// Ahmed", "Sayed", "Ali", "Osama", "Gamal","Ameer"  "samer", "sameh" "haytham", "Shady" "Gamal" "Sayad" 
+
+//  console.log(allfriends)
+
+//  console.log(allfriends.join());
+//  console.log(allfriends.join(""));
+//  console.log(allfriends.join(" @ "));
+//  console.log(allfriends.join("|"));
+//  console.log(allfriends.join("|").toUpperCase());
+ 
+
+ /*
+ let mody = 0;
+
+ let counter = 3;
+
+ let my = ["ahmed", "mody", "mohamed", "elmah", "magalos", "amer"];
+
+console.log(my);
+
+ console.log(my.slice("????"));
+
+ console.log();
+
+ console.log();
+ */
+
+// let colors = ["Red", "Green", "Black"];
+
+// let models = [2020, 2021];
+
+// for (let i = 0; i < products.length; j++) {
+//     console.log("#".repeat(15))
+//     console.log(`# ${products[i]}`);
+//     console.log("#".repeat(15));
+//     console.log("colors: ");
+// }
+// for (let j = 0; j < colors.length; j++){
+//     console.log(`- ${colors[j]} `);
+
+// }
+// console.log("models: ");
+// for (let k = 0; k < models.length; k++) {
+//     console.log(` - ${models[k]}`);
+
+
+// }
+
+// let myAdmins = ["ahmed", "Osama", "Sayad", "Stop", "Samera"];
+// let myElmployees = ["Amgad", "samah", "Ameer", "Omar", "Othman", "Amany", "Samia"];
+
+// document.write(`<div>We Have X Admins</div>`);
+
+function sayMessage(FName, Iname) {
+    let massage = "hallo" ;
+}
+function concatmsg () {
+    massage = `${mesage} -*`
+
+}
