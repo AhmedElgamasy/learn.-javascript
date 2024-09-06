9e23abbe4a17eaf54b3047376fc802d2f2c6b4fc
ahmedelgamasy <ahmedalgamasyy@gmail.com>
2024-09-06 21:58:50 +0300

    6/9/24

--- main.js
+++ main.js
@@ -1,232 +1,295 @@
-//
-//console.log(10== "10");
-//console.log(-100 == "-100");
-//console.log(10 != "10");
-//
-//console.log(10 === "10");
-//console.log(10 !== "10");
-//console.log(10 !== 10);
-//console.log(10 > 20);
-//console.log(10 > 10);
-//console.log(10 >= 10);
-//console.log(10 <= 10);
-//
-//
-//console.log(true);
-//console.log(!true);
-//
-//console.log((10 == "10"));
-//
-//console.log(10 == "10" && 10 > 8 && 10 > 50);
-//
-//console.log(10 == "10" || 10 > 8 || 10 > 50);
-//
-//
-//let price = 100; 
-//let discount = true;
-//let discountAmount = 30;
-//
-//console.log(price);    
-//
-//if (discount === true) {
-//  price -= discountAmount;
-//  
-//  price = price -  discountAmount;
-//
-//console.log(price);
-//
-//
-//let price = 100;
-//let discount = false;
-//let discountAmount = 30;
-//let country = "Egypt";
-//let student = true;
-//
-//if (discount === true ) {
-//
-//    price -= discountAmount;
-//
-//} else if (country === "Egypt") {
-//    price -= discountAmount + 10 ;
-//
-//
-//
-//} else{
-//
-//    price -= 10;
-//}
-//
-//console.log(price) 
-//
-
-
-// let theName = "Ahmed";
-// let theGender = "Male";
-// let theAge = 30; 
-
-// if (theGender === "male" ){
-//     console.log("Mr");
-// } else {
-//     console.log("Mrs");
+        //
+        //console.log(10== "10");
+         //console.log(-100 == "-100");
+         //console.log(10 != "10");
+         //
+      //console.log(10 === "10");
+       //console.log(10 !== "10");
+        //console.log(10 !== 10);
+         //console.log(10 > 20);
+            //console.log(10 > 10);
+            //console.log(10 >= 10);
+            //console.log(10 <= 10);
+            //
+            //
+     //console.log(true);
+     //console.log(!true);
+      //
+       //console.log((10 == "10"));
+        //
+        //console.log(10 == "10" && 10 > 8 && 10 > 50);
+           //
+            //console.log(10 == "10" || 10 > 8 || 10 > 50);
+            //
+             //
+              //let price = 100; 
+                //let discount = true;
+                 //let discountAmount = 30;
+                  //
+                   //console.log(price);    
+                    //
+                     //if (discount === true) {
+                      //  price -= discountAmount;
+                       //  
+                  //  price = price -  discountAmount;
+                   //
+                //console.log(price);
+                 //
+            //
+             //let price = 100;
+             //let discount = false;
+              //let discountAmount = 30;
+               //let country = "Egypt";
+                //let student = true;
+                 //
+                  //if (discount === true ) {
+                   //
+                   //    price -= discountAmount;
+                //
+            //} else if (country === "Egypt") {
+        //    price -= discountAmount + 10 ;
+        //
+        //
+        //
+        //} else{
+        //
+        //    price -= 10;
+        //}
+        //
+        //console.log(price) 
+        //
+        
+        
+        // let theName = "Ahmed";
+        // let theGender = "Male";
+        // let theAge = 30; 
+        
+        // if (theGender === "male" ){
+        //     console.log("Mr");
+        // } else {
+        //     console.log("Mrs");
+        // }
+        
+        // theGender === "Male" ? console.log("Mr") : console.log("Mrs");
+        
+        
+        // let mybro = ["Ahmed", "Sayed", "Ali", "Osama", "Gamal","Ameer" ];
+        // console.log(mybro)
+        // console.log(mybro.slice());
+        // console.log(mybro.slice(1, 3));
+        // console.log(mybro.slice(-3));
+        // console.log(mybro.slice(1, -2));
+        
+        
+        
+        //  let myfriends = ["Ahmed" ];
+        //  let mynewfrinds = ["samer", "sameh" ];
+        //  let schoolfriends = ["haytham", "Shady"];
+        
+        //  let allfriends = myfriends.concat(mynewfrinds, schoolfriends, "Gamal", schoolfriends.slice(1, 3));
+        
+        // Ahmed", "Sayed", "Ali", "Osama", "Gamal","Ameer"  "samer", "sameh" "haytham", "Shady" "Gamal" "Sayad" 
+        
+        //  console.log(allfriends)
+        
+        //  console.log(allfriends.join());
+        //  console.log(allfriends.join(""));
+        //  console.log(allfriends.join(" @ "));
+        //  console.log(allfriends.join("|"));
+        //  console.log(allfriends.join("|").toUpperCase());
+        
+        
+         /*
+         let mody = 0;
+        
+         let counter = 3;
+        
+         let my = ["ahmed", "mody", "mohamed", "elmah", "magalos", "amer"];
+        
+        console.log(my);
+        
+         console.log(my.slice("????"));
+        
+         console.log();
+        
+         console.log();
+         */
+        
+        // let colors = ["Red", "Green", "Black"];
+        
+        // let models = [2020, 2021];
+        
+        // for (let i = 0; i < products.length; j++) {
+        //     console.log("#".repeat(15))
+        //     console.log(`# ${products[i]}`);
+        //     console.log("#".repeat(15));
+        //     console.log("colors: ");
+        // }
+        // for (let j = 0; j < colors.length; j++){
+        //     console.log(`- ${colors[j]} `);
+        
+        // }
+        // console.log("models: ");
+        // for (let k = 0; k < models.length; k++) {
+        //     console.log(` - ${models[k]}`);
+        //
+        //
+        //// }
+        //
+        // let myAdmins = ["ahmed", "Osama", "Sayad", "Stop", "Samera"];
+        // let myElmployees = ["Amgad", "samah", "Ameer", "Omar", "Othman", "Amany", "Samia"];
+        //
+        // document.write(`<div>We Have X Admins</div>`);
+        //
+        //function sayMessage(FName, Iname) {
+        //    let massage = "hallo" ;
+        //}
+        //function concatmsg () {
+        //    massage = `${mesage} ${locked} `  
+        //
+        //}
+        //
+        //function print() {
+        //    return 10;
+        //}
+        //let print =  function (){
+        //    return 90;
+        //}
+        //let names = function () {
+        //
+        //  return "???";
+        //}
+        //
+        //console.log(names("osama", "mohamed"));
+        //
+        //let myNums = [1, 2, 3, 4, 5 ,6];
+        //
+        //let newArray = []
+        //for (let i = 0; i < myNums.length; i++) {
+        //  newArray.push(myNums[i] + myNums[i]);
+        //}
+        //console.log(newArray)
+        //
+        //let addSelf = myNums.map(function(el, index, arr) {
+        //
+        //},10)
+        //
+        //showDetails("Osama", 38, true);
+        //
+        //let print = () => 10;
+        //
+        //console.log(print());
+        //
+        //let print = (num) => num;
+        //
+        //console.log(print(100));
+        //
+        //
+        //var a = 1;
+        //let b = 2;
+        //
+        //function showtext() {
+        //    var a = 10;
+        //    let b = 20;
+        //}
+        //console.
+        //var x = 10;
+        //if (10 === 10) {
+        //    let x = 50;
+        //} 
+        //
+        //console.log(x);
+        //let swappingCases = "mODY";
+        //let invertNumbers = [1, -2 , -300 ,15 ,37 , -34];
+        //let ignoreBooleans = "ELzero123moo";
+        //
+        //let sw = swappingCases
+        //.split("")
+        //.map(function (ele) {
+        //return ele === ele.toUpperCase() ? ele.toLocaleLowerCase() : ele.toLocaleLowerCase();
+        //})
+        //.join("")
+        //
+        //console.log(sw)
+        //
+        //let inv = invertNumbers.map(function (ele) {
+        //  return - ele;
+        //})
+        //
+        //let theBiggest = ["bla", "Propaganda", "Other", "AAA", "Battery"]
+        //let mymainelement = document.createElement("div");
+        //let mymainelement = document.createElement("h2");
+        //let mymainelement = document.createElement("p");
+//let myP = document.querySelector("p");
+// 
+// myP.onclick = function () {
+//     console.log("Massage From OnClick");
 // }
-
-// theGender === "Male" ? console.log("Mr") : console.log("Mrs");
-
-
-// let mybro = ["Ahmed", "Sayed", "Ali", "Osama", "Gamal","Ameer" ];
-// console.log(mybro)
-// console.log(mybro.slice());
-// console.log(mybro.slice(1, 3));
-// console.log(mybro.slice(-3));
-// console.log(mybro.slice(1, -2));
- 
-  
- 
-//  let myfriends = ["Ahmed" ];
-//  let mynewfrinds = ["samer", "sameh" ];
-//  let schoolfriends = ["haytham", "Shady"];
-
-//  let allfriends = myfriends.concat(mynewfrinds, schoolfriends, "Gamal", schoolfriends.slice(1, 3));
-
-// Ahmed", "Sayed", "Ali", "Osama", "Gamal","Ameer"  "samer", "sameh" "haytham", "Shady" "Gamal" "Sayad" 
-
-//  console.log(allfriends)
-
-//  console.log(allfriends.join());
-//  console.log(allfriends.join(""));
-//  console.log(allfriends.join(" @ "));
-//  console.log(allfriends.join("|"));
-//  console.log(allfriends.join("|").toUpperCase());
- 
-
- /*
- let mody = 0;
-
- let counter = 3;
-
- let my = ["ahmed", "mody", "mohamed", "elmah", "magalos", "amer"];
-
-console.log(my);
-
- console.log(my.slice("????"));
-
- console.log();
-
- console.log();
- */
-
-// let colors = ["Red", "Green", "Black"];
-
-// let models = [2020, 2021];
-
-// for (let i = 0; i < products.length; j++) {
-//     console.log("#".repeat(15))
-//     console.log(`# ${products[i]}`);
-//     console.log("#".repeat(15));
-//     console.log("colors: ");
+// 
+// function one() {
+// 
+//     console.log("Massage from OnClick 1");
 // }
-// for (let j = 0; j < colors.length; j++){
-//     console.log(`- ${colors[j]} `);
+//     console.log(location);
+//     console.log(location.href);
+//  setTimeout(function () {
+//  window.open("https://google.com","_blank","width=400,height=400,left=100, top,200");
+// }, 2000);
+// history.object()
+// let lis = document.querySelectorAll("ul li");
+// let exp = document.querySelector(".experiment");
+//
+//    lis.forEach((li) => {
+//        li.addEventlistener("click", (e) => {
+//
+//lis.forEach((li) => {
+//    li.classList.remove("active");
+//});
+//e.currentarget.classList.add("active");
+//window.localstorage.setItem("color", e.currentTarget.dataset.color);
+//exp.style.backgroundcolor = e.currentTarget.dataset.color;
+//    });
+//
+//});
+let book = "Video";
+let Video = "Book";
 
-// }
-// console.log("models: ");
-// for (let k = 0; k < models.length; k++) {
-//     console.log(` - ${models[k]}`);
-//
-//
-//// }
-//
-// let myAdmins = ["ahmed", "Osama", "Sayad", "Stop", "Samera"];
-// let myElmployees = ["Amgad", "samah", "Ameer", "Omar", "Othman", "Amany", "Samia"];
-//
-// document.write(`<div>We Have X Admins</div>`);
-//
-//function sayMessage(FName, Iname) {
-//    let massage = "hallo" ;
-//}
-//function concatmsg () {
-//    massage = `${mesage} ${locked} `  
-//
-//}
-//
-//function print() {
-//    return 10;
-//}
-//let print =  function (){
-//    return 90;
-//}
-//let names = function () {
-//
-//  return "???";
-//}
-//
-//console.log(names("osama", "mohamed"));
-//
-//let myNums = [1, 2, 3, 4, 5 ,6];
-//
-//let newArray = []
-//for (let i = 0; i < myNums.length; i++) {
-//  newArray.push(myNums[i] + myNums[i]);
-//}
-//console.log(newArray)
-//
-//let addSelf = myNums.map(function(el, index, arr) {
-//
-//},10)
-//
-//showDetails("Osama", 38, true);
-//
-//let print = () => 10;
-//
-//console.log(print());
-//
-//let print = (num) => num;
-//
-//console.log(print(100));
-//
-//
-//var a = 1;
-//let b = 2;
-//
-//function showtext() {
-//    var a = 10;
-//    let b = 20;
-//}
-//console.
-//var x = 10;
-//if (10 === 10) {
-//    let x = 50;
-//} 
-//
-//console.log(x);
-//let swappingCases = "mODY";
-//let invertNumbers = [1, -2 , -300 ,15 ,37 , -34];
-//let ignoreBooleans = "ELzero123moo";
-//
-//let sw = swappingCases
-//.split("")
-//.map(function (ele) {
-//return ele === ele.toUpperCase() ? ele.toLocaleLowerCase() : ele.toLocaleLowerCase();
-//})
-//.join("")
-//
-//console.log(sw)
-//
-//let inv = invertNumbers.map(function (ele) {
-//  return - ele;
-//})
-//
-//let theBiggest = ["bla", "Propaganda", "Other", "AAA", "Battery"]
-//let mymainelement = document.createElement("div");
-//let mymainelement = document.createElement("h2");
-//let mymainelement = document.createElement("p");
-let myP = document.querySelector("p");
+let stash = book;
 
-myP.onclick = function () {
-    console.log("Massage From OnClick");
+book = Video; 
+
+Video = stash; 
+
+[book,video] = [Video,book]
+
+console.log(book);
+console.log(Video);
+
+
+const user = {
+    theName: "Mody",
+    theAge:  14,
+    theTitle: "Developer", 
+    thecountry: "Egypt",
 }
 
-function one() {
+console.log(user.theName);
+console.log(user.theAge);
+console.log(user.theTitle);
+console.log(user.thecountry);
 
-    console.log("Massage from OnClick 1");
+
+const User = {
+    theName: "Mody",
+    theAge:  14,
+    theTitle: "Developer", 
+    thecountry: "Egypt",
+    thecolor: "black",
 }
+
+const { theName: n, theAge: a, thecountry, thecolor: co = "red" } = user; 
+
+console.log(n);
+console.log(a);
+console.log(user.thecountry);
+console.log(co);
